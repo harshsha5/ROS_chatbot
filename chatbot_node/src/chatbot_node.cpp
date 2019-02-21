d1184430b43f202441e2796ddce5bb5a9f68b100
harsha5 <harshbitsp@gmail.com>
2019-02-21 07:58:05 -0500

    Publish on subscribe fixed

--- chatbot_node/src/chatbot_node.cpp
+++ chatbot_node/src/chatbot_node.cpp
@@ -2,27 +2,44 @@
 #include <chatbot_node/reply_msg.h>
 #include <message_ui/sent_msg.h>
 #include <string>
+#include <ros/console.h>
 
 using namespace std;
 
+chatbot_node::reply_msg msg;
+ros::Publisher chatter_pub;
+
+
 //Add your code here
+
+void chatterCallback(const message_ui::sent_msg::ConstPtr& msg_from_ui)
+{
+  msg.header = msg_from_ui->header;
+  if (msg_from_ui->message.compare("Hello") == 0)
+    msg.message = "Hello Raju";
+  else if((msg_from_ui->message.compare("What is your name?") == 0))
+    msg.message = "My name is MRSD Siri";
+  else if((msg_from_ui->message.compare("How are you?") == 0))
+    msg.message = "I'm fine. Thank you!";
+  else
+  {
+    msg.message = "Hmm.. I'm not sure.";
+  }
+  
+  chatter_pub.publish(msg);
+}
 
 int main(int argc, char **argv) {
 
   ros::init(argc, argv, "chatbot_node");
   ros::NodeHandle n;
 
-  //Add your code here
-  ros::Publisher chatter_pub = n.advertise<chatbot_node::reply_msg>("reply_msg", 500);
+  chatter_pub = n.advertise<chatbot_node::reply_msg>("reply_msg", 1);
+  ros::Subscriber sub = n.subscribe("sent_msg", 500, chatterCallback);
 
   ros::Rate loop_rate(20);
 
   while(ros::ok()) {
-    chatbot_node::reply_msg msg;
-    //msg.header = ;
-    //msg.message = ;
-    
-    chatter_pub.publish(msg);
     ros::spinOnce();
     loop_rate.sleep();
   }
