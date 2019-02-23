390e9a2484178483a1365b9b0aa5c38a628e93c0
harsha5 <harshbitsp@gmail.com>
2019-02-23 13:15:31 -0500

    Arithmetic node completed. Chatbot node bugs fixed

--- chatbot_node/src/chatbot_node.cpp
+++ chatbot_node/src/chatbot_node.cpp
@@ -13,24 +13,28 @@ string naam;
 //Add your code here
 
 void chatterCallback(const message_ui::sent_msg::ConstPtr& msg_from_ui)
-{
+{ 
+  int flag=0;
   msg.header = msg_from_ui->header;
   if (msg_from_ui->message.compare("Hello") == 0)
   {
     ros::param::get("/name",naam);
-
     msg.message = "Hello " + naam;
+    flag++;
   }
   else if((msg_from_ui->message.compare("What is your name?") == 0))
-    msg.message = "My name is MRSD Siri";
+    {
+      msg.message = "My name is MRSD Siri";
+      flag++;
+    } 
   else if((msg_from_ui->message.compare("How are you?") == 0))
-    msg.message = "I'm fine. Thank you!";
-  else
-  {
-    msg.message = "Hmm.. I'm not sure.";
-  }
-  
-  chatter_pub.publish(msg);
+    {
+      msg.message = "I'm fine. Thank you!";
+      flag++;
+    }
+  if(flag==1)  
+    chatter_pub.publish(msg);
+    
 }
 
 int main(int argc, char **argv) {
